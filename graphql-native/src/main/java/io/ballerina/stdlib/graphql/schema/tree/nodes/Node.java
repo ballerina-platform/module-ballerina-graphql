3e445381289a01c81fef13cc84c15f017d619a10
Thisaru Guruge <thisaru.guruge@gmail.com>
2021-03-03 01:30:53 +0530

    Remove redundant class and varriables

--- graphql-native/src/main/java/io/ballerina/stdlib/graphql/schema/tree/nodes/Node.java
+++ graphql-native/src/main/java/io/ballerina/stdlib/graphql/schema/tree/nodes/Node.java
@@ -18,19 +18,51 @@
 
 package io.ballerina.stdlib.graphql.schema.tree.nodes;
 
+import io.ballerina.runtime.api.types.Type;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Represents a node in the tree data structure used in Ballerina GraphQL schema generator.
+ * Represents a node in a tree used in Ballerina GraphQL schema generation.
  *
  * @since 0.2.0
  */
-public abstract class Node {
-    String name;
+public class Node {
+    private String name;
+    private Type type;
+    private Map<String, Node> children;
 
     public Node(String name) {
+        this(name, null);
+    }
+    public Node(String name, Type type) {
         this.name = name;
+        this.type = type;
+        this.children = new HashMap<>();
+    }
+
+    public Type getType() {
+        return this.type;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public void addChild(Node child) {
+        this.children.put(child.getName(), child);
+    }
+
+    public Node getChild(String name) {
+        return this.children.get(name);
+    }
+
+    public boolean hasChild(String name) {
+        return this.children.containsKey(name);
+    }
+
+    public Map<String, Node> getChildren() {
+        return this.children;
     }
 }
