32d67c4e45f768a56062bc7de8a7970898e0b190
Thisaru Guruge <thisaru.guruge@gmail.com>
2021-03-24 14:53:36 +0530

    Get type symbol for listener initialization

--- graphql-compiler-plugin/src/main/java/io/ballerina/stdlib/graphql/compiler/GraphqlServiceValidator.java
+++ graphql-compiler-plugin/src/main/java/io/ballerina/stdlib/graphql/compiler/GraphqlServiceValidator.java
@@ -18,13 +18,30 @@
 
 package io.ballerina.stdlib.graphql.compiler;
 
+import io.ballerina.compiler.api.SemanticModel;
+import io.ballerina.compiler.api.symbols.FunctionSymbol;
+import io.ballerina.compiler.api.symbols.ModuleSymbol;
+import io.ballerina.compiler.api.symbols.SymbolKind;
+import io.ballerina.compiler.api.symbols.TypeDescKind;
+import io.ballerina.compiler.api.symbols.TypeReferenceTypeSymbol;
+import io.ballerina.compiler.api.symbols.TypeSymbol;
+import io.ballerina.compiler.api.symbols.VariableSymbol;
+import io.ballerina.compiler.syntax.tree.ExpressionNode;
 import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
+import io.ballerina.compiler.syntax.tree.SyntaxKind;
+import io.ballerina.compiler.syntax.tree.UnionTypeDescriptorNode;
+import io.ballerina.projects.DocumentId;
+import io.ballerina.projects.Module;
 import io.ballerina.projects.plugins.AnalysisTask;
 import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
 import io.ballerina.tools.diagnostics.Diagnostic;
 import io.ballerina.tools.diagnostics.DiagnosticFactory;
 import io.ballerina.tools.diagnostics.DiagnosticInfo;
 import io.ballerina.tools.diagnostics.DiagnosticSeverity;
+import io.ballerina.tools.diagnostics.Location;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Validates a Ballerina GraphQL Service.
@@ -34,10 +51,35 @@ public class GraphqlServiceValidator implements AnalysisTask<SyntaxNodeAnalysisC
     @Override
     public void perform(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext) {
         ServiceDeclarationNode serviceDeclarationNode = (ServiceDeclarationNode) syntaxNodeAnalysisContext.node();
-
-        DiagnosticInfo diagnosticInfo = new DiagnosticInfo("GRAPHQL_101", "service declaration",
+        TypeSymbol typeSymbol = getListenerTypeSymbol(syntaxNodeAnalysisContext);
+        DiagnosticInfo diagnosticInfo = new DiagnosticInfo("GRAPHQL_101", typeSymbol.signature(),
                                                            DiagnosticSeverity.INFO);
         Diagnostic diagnostic = DiagnosticFactory.createDiagnostic(diagnosticInfo, serviceDeclarationNode.location());
         syntaxNodeAnalysisContext.reportDiagnostic(diagnostic);
+    }
+
+    private TypeSymbol getListenerTypeSymbol(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext) {
+        ServiceDeclarationNode serviceDeclarationNode = (ServiceDeclarationNode) syntaxNodeAnalysisContext.node();
+        TypeSymbol moduleType = null;
+        for (ExpressionNode expressionNode : serviceDeclarationNode.expressions()) {
+            if (expressionNode.kind() == SyntaxKind.EXPLICIT_NEW_EXPRESSION) {
+                SemanticModel semanticModel = syntaxNodeAnalysisContext.semanticModel();
+                Module defaultModule = syntaxNodeAnalysisContext.currentPackage().getDefaultModule();
+                DocumentId documentId = syntaxNodeAnalysisContext.documentId();
+                moduleType = ((TypeReferenceTypeSymbol) (semanticModel.symbol(defaultModule.document(documentId),
+                         expressionNode.lineRange().startLine()).get())).typeDescriptor();
+
+            } else if (expressionNode.kind() == SyntaxKind.SIMPLE_NAME_REFERENCE) {
+                VariableSymbol symbol = (VariableSymbol) syntaxNodeAnalysisContext.semanticModel().symbol
+                        (syntaxNodeAnalysisContext.currentPackage().getDefaultModule().
+                                 document(syntaxNodeAnalysisContext.documentId()),
+                         expressionNode.lineRange().startLine()).get();
+                moduleType = symbol.typeDescriptor();
+            } else {
+                moduleType = null;
+                // todo
+            }
+        }
+        return moduleType;
     }
 }
