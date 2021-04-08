577aa4d44b2dc1581697c9db7e357e5a60ccbb0a
TharindaDilshan <tharindad7@gmail.com>
2021-04-08 21:38:28 +0530

    Add validation to check the presence of remote functions

--- graphql-compiler-plugin/src/main/java/io/ballerina/stdlib/graphql/compiler/GraphqlServiceValidator.java
+++ graphql-compiler-plugin/src/main/java/io/ballerina/stdlib/graphql/compiler/GraphqlServiceValidator.java
@@ -18,18 +18,16 @@
 
 package io.ballerina.stdlib.graphql.compiler;
 
-import io.ballerina.compiler.api.SemanticModel;
+import io.ballerina.compiler.api.symbols.ModuleSymbol;
 import io.ballerina.compiler.api.symbols.ServiceDeclarationSymbol;
-import io.ballerina.compiler.api.symbols.SymbolKind;
+import io.ballerina.compiler.api.symbols.Symbol;
 import io.ballerina.compiler.api.symbols.TypeDescKind;
 import io.ballerina.compiler.api.symbols.TypeReferenceTypeSymbol;
 import io.ballerina.compiler.api.symbols.TypeSymbol;
-import io.ballerina.compiler.api.symbols.VariableSymbol;
-import io.ballerina.compiler.syntax.tree.ExpressionNode;
+import io.ballerina.compiler.api.symbols.UnionTypeSymbol;
+import io.ballerina.compiler.syntax.tree.FunctionDefinitionNode;
 import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
 import io.ballerina.compiler.syntax.tree.SyntaxKind;
-import io.ballerina.projects.DocumentId;
-import io.ballerina.projects.Module;
 import io.ballerina.projects.plugins.AnalysisTask;
 import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
 import io.ballerina.tools.diagnostics.Diagnostic;
@@ -38,6 +36,7 @@ import io.ballerina.tools.diagnostics.DiagnosticInfo;
 import io.ballerina.tools.diagnostics.DiagnosticSeverity;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Validates a Ballerina GraphQL Service.
@@ -47,49 +46,65 @@ public class GraphqlServiceValidator implements AnalysisTask<SyntaxNodeAnalysisC
     @Override
     public void perform(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext) {
         ServiceDeclarationNode serviceDeclarationNode = (ServiceDeclarationNode) syntaxNodeAnalysisContext.node();
-        getListenerTypes(syntaxNodeAnalysisContext);
-        TypeSymbol typeSymbol = getListenerTypeSymbol(syntaxNodeAnalysisContext);
-        DiagnosticInfo diagnosticInfo = new DiagnosticInfo("GRAPHQL_101", typeSymbol.signature(),
-                                                           DiagnosticSeverity.INFO);
-        Diagnostic diagnostic = DiagnosticFactory.createDiagnostic(diagnosticInfo, serviceDeclarationNode.location());
-        syntaxNodeAnalysisContext.reportDiagnostic(diagnostic);
-    }
-
-    private void getListenerTypes(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext) {
-        ServiceDeclarationNode node = (ServiceDeclarationNode) syntaxNodeAnalysisContext.node();
-        ServiceDeclarationSymbol symbol = (ServiceDeclarationSymbol) syntaxNodeAnalysisContext.semanticModel().symbol(
-                node).get();
-        List<TypeSymbol> listenerTypes = symbol.listenerTypes();
-        for (TypeSymbol listenerType : listenerTypes) {
-            if (listenerType.kind() == SymbolKind.) {
-                listenerType.signature();
+        Optional<Symbol> serviceDeclarationSymbol = syntaxNodeAnalysisContext.semanticModel()
+                .symbol(serviceDeclarationNode);
+        if (serviceDeclarationSymbol.isPresent()) {
+            List<TypeSymbol> listenerTypes =
+                    ((ServiceDeclarationSymbol) serviceDeclarationSymbol.get()).listenerTypes();
+            for (TypeSymbol listenerType : listenerTypes) {
+                if (isListenerBelongsToGraphQLModule(listenerType)) {
+                    validate(syntaxNodeAnalysisContext);
+                }
             }
         }
     }
 
-    private TypeSymbol getListenerTypeSymbol(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext) {
+    public static boolean equals(String actual, String expected) {
+        return actual.compareTo(expected) == 0;
+    }
+
+    private boolean isGraphQLModule(ModuleSymbol moduleSymbol) {
+        return equals(moduleSymbol.getName().get(), "graphql")
+                && equals(moduleSymbol.id().orgName(), "ballerina");
+    }
+
+    private boolean isListenerBelongsToGraphQLModule(TypeSymbol listenerType) {
+        if (listenerType.typeKind() == TypeDescKind.UNION) {
+            return ((UnionTypeSymbol) listenerType).memberTypeDescriptors().stream()
+                    .filter(typeDescriptor -> typeDescriptor instanceof TypeReferenceTypeSymbol)
+                    .map(typeReferenceTypeSymbol -> (TypeReferenceTypeSymbol) typeReferenceTypeSymbol)
+                    .anyMatch(typeReferenceTypeSymbol -> isGraphQLModule(typeReferenceTypeSymbol.getModule().get()));
+        }
+
+        if (listenerType.typeKind() == TypeDescKind.TYPE_REFERENCE) {
+            return isGraphQLModule(((TypeReferenceTypeSymbol) listenerType).typeDescriptor().getModule().get());
+        }
+
+        return false;
+    }
+
+    public void validate(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext) {
         ServiceDeclarationNode serviceDeclarationNode = (ServiceDeclarationNode) syntaxNodeAnalysisContext.node();
-        TypeSymbol moduleType = null;
-        for (ExpressionNode expressionNode : serviceDeclarationNode.expressions()) {
-            if (expressionNode.kind() == SyntaxKind.EXPLICIT_NEW_EXPRESSION) {
-                SemanticModel semanticModel = syntaxNodeAnalysisContext.semanticModel();
-                Module defaultModule = syntaxNodeAnalysisContext.currentPackage().getDefaultModule();
-                DocumentId documentId = syntaxNodeAnalysisContext.documentId();
-                moduleType = ((TypeReferenceTypeSymbol) (semanticModel.symbol(defaultModule.document(documentId),
-                                                                              expressionNode.lineRange().startLine())
-                        .get())).typeDescriptor();
+        serviceDeclarationNode.members().stream()
+                .filter(child -> child.kind() == SyntaxKind.OBJECT_METHOD_DEFINITION
+                        || child.kind() == SyntaxKind.RESOURCE_ACCESSOR_DEFINITION).forEach(node -> {
+            checkForRemoteMethods((FunctionDefinitionNode) node, syntaxNodeAnalysisContext);
+        });
+    }
 
-            } else if (expressionNode.kind() == SyntaxKind.SIMPLE_NAME_REFERENCE) {
-                VariableSymbol symbol = (VariableSymbol) syntaxNodeAnalysisContext.semanticModel().symbol
-                        (syntaxNodeAnalysisContext.currentPackage().getDefaultModule().
-                                 document(syntaxNodeAnalysisContext.documentId()),
-                         expressionNode.lineRange().startLine()).get();
-                moduleType = symbol.typeDescriptor();
-            } else {
-                moduleType = null;
-                // todo
-            }
+    private void checkForRemoteMethods(FunctionDefinitionNode node, SyntaxNodeAnalysisContext context) {
+        int hasRemoteKeyword = node.qualifierList().stream()
+                .filter(q -> q.kind() == SyntaxKind.RESOURCE_KEYWORD && q.isMissing()).toArray().length;
+
+        if (hasRemoteKeyword > 0) {
+            reportDiagnostic(node, "GRAPHQL_101", "REMOTE_METHODS_ARE_NOT_ALLOWED", context);
         }
-        return moduleType;
+    }
+
+    private void reportDiagnostic(FunctionDefinitionNode node, String code, String messageFormat,
+                                  SyntaxNodeAnalysisContext syntaxNodeAnalysisContext) {
+        DiagnosticInfo diagnosticInfo = new DiagnosticInfo(code, messageFormat, DiagnosticSeverity.INFO);
+        syntaxNodeAnalysisContext.reportDiagnostic(DiagnosticFactory.createDiagnostic(diagnosticInfo,
+                                                                node.location(), node.functionName().toString()));
     }
 }
