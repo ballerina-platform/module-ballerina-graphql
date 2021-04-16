419a3f303346c255bb9a3446716c3ad7744aa299
“aashikam” <arshika.nm@gmail.com>
2021-04-16 13:21:16 +0530

    Add listener type validations

---
+++ graphql-compiler-plugin/src/main/java/io/ballerina/stdlib/graphql/compiler/GraphqlServiceAnalysisTask.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.stdlib.graphql.compiler;
+
+import io.ballerina.compiler.api.SemanticModel;
+import io.ballerina.compiler.api.symbols.ModuleSymbol;
+import io.ballerina.compiler.api.symbols.ServiceDeclarationSymbol;
+import io.ballerina.compiler.api.symbols.Symbol;
+import io.ballerina.compiler.api.symbols.TypeDescKind;
+import io.ballerina.compiler.api.symbols.TypeSymbol;
+import io.ballerina.compiler.api.symbols.UnionTypeSymbol;
+import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
+import io.ballerina.projects.plugins.AnalysisTask;
+import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
+import io.ballerina.stdlib.graphql.compiler.PluginConstants.CompilationErrors;
+import io.ballerina.tools.diagnostics.DiagnosticSeverity;
+
+import java.util.List;
+import java.util.Optional;
+
+import static io.ballerina.stdlib.graphql.compiler.PluginUtils.validateModuleId;
+
+/**
+ * Validates a Ballerina GraphQL Service.
+ */
+public class GraphqlServiceAnalysisTask implements AnalysisTask<SyntaxNodeAnalysisContext> {
+
+    public GraphqlServiceAnalysisTask() {
+    }
+
+    @Override
+    public void perform(SyntaxNodeAnalysisContext context) {
+        if (!isGraphQlService(context)) {
+            return;
+        }
+    }
+
+    private boolean isGraphQlService(SyntaxNodeAnalysisContext context) {
+        boolean isGraphQlService = false;
+        SemanticModel semanticModel = context.semanticModel();
+        ServiceDeclarationNode serviceDeclarationNode = (ServiceDeclarationNode) context.node();
+        Optional<Symbol> symbol = semanticModel.symbol(serviceDeclarationNode);
+        if (symbol.isPresent()) {
+            ServiceDeclarationSymbol serviceDeclarationSymbol = (ServiceDeclarationSymbol) symbol.get();
+            List<TypeSymbol> listeners = serviceDeclarationSymbol.listenerTypes();
+            if (listeners.size() > 1) {
+                context.reportDiagnostic(PluginUtils.getDiagnostic(CompilationErrors.INVALID_MULTIPLE_LISTENERS,
+                        DiagnosticSeverity.ERROR, serviceDeclarationNode.location()));
+            } else {
+                if (listeners.get(0).typeKind() == TypeDescKind.UNION) {
+                    UnionTypeSymbol unionTypeSymbol = (UnionTypeSymbol) listeners.get(0);
+                    List<TypeSymbol> members = unionTypeSymbol.memberTypeDescriptors();
+                    for (TypeSymbol memberSymbol : members) {
+                        Optional<ModuleSymbol> module = memberSymbol.getModule();
+                        if (module.isPresent()) {
+                            isGraphQlService = validateModuleId(module.get());
+                        }
+                    }
+                } else {
+                    Optional<ModuleSymbol> module = listeners.get(0).getModule();
+                    if (module.isPresent()) {
+                        isGraphQlService = validateModuleId(module.get());
+                    }
+                }
+            }
+        }
+        return isGraphQlService;
+    }
+}
---
+++ graphql-compiler-plugin/src/main/java/io/ballerina/stdlib/graphql/compiler/PluginUtils.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerina.stdlib.graphql.compiler;
+
+import io.ballerina.compiler.api.symbols.ModuleSymbol;
+import io.ballerina.tools.diagnostics.Diagnostic;
+import io.ballerina.tools.diagnostics.DiagnosticFactory;
+import io.ballerina.tools.diagnostics.DiagnosticInfo;
+import io.ballerina.tools.diagnostics.DiagnosticSeverity;
+import io.ballerina.tools.diagnostics.Location;
+
+/**
+ * Util class for the compiler plugin.
+ */
+public class PluginUtils {
+    public static Diagnostic getDiagnostic(PluginConstants.CompilationErrors error,
+                                           DiagnosticSeverity severity, Location location) {
+        String errorMessage = error.getError();
+        String diagnosticCode = error.getErrorCode();
+        DiagnosticInfo diagnosticInfo = new DiagnosticInfo(diagnosticCode, errorMessage, severity);
+        return DiagnosticFactory.createDiagnostic(diagnosticInfo, location);
+    }
+
+    public static boolean validateModuleId(ModuleSymbol moduleSymbol) {
+        String moduleName = moduleSymbol.id().moduleName();
+        String orgName = moduleSymbol.id().orgName();
+        return moduleName.equals(PluginConstants.PACKAGE_PREFIX) &&
+                orgName.equals(PluginConstants.PACKAGE_ORG);
+    }
+}
