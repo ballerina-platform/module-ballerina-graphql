3e587e2671b9e03f7991517e1735b4ca03935270
MohamedSabthar <sabthar@wso2.com>
2023-01-03 15:49:01 +0530

    Use single quotes to quote the type name in error messages

--- compiler-plugin/src/main/java/io/ballerina/stdlib/graphql/compiler/diagnostics/DiagnosticMessage.java
+++ compiler-plugin/src/main/java/io/ballerina/stdlib/graphql/compiler/diagnostics/DiagnosticMessage.java
@@ -26,50 +26,51 @@ import static io.ballerina.stdlib.graphql.compiler.service.validator.ValidatorUt
  * Compilation error messages used in Ballerina GraphQL package compiler plugin.
  */
 public enum DiagnosticMessage {
-    ERROR_101("Service class `{0}` contains remote method `{1}`. Remote methods are not allowed inside the service "
+    ERROR_101("Service class ''{0}'' contains remote method ''{1}''. Remote methods are not allowed inside the service "
                       + "classes returned from GraphQL resources"),
-    ERROR_102("Invalid GraphQL field Type `{0}` provided for the GraphQL field `{1}`"),
-    ERROR_103("Invalid input parameter type `{0}` used in GraphQL field `{1}`"),
-    ERROR_104("Return type not provided for the GraphQL field `{0}`"),
-    ERROR_105("The GraphQL field `{0}` has a ballerina union type, but the union does not contain any valid data type "
-                      + "to return"),
-    ERROR_106("Invalid resource method accessor `{0}` used in method `{1}`. Only " + RESOURCE_FUNCTION_GET + " "
-                      + "allowed"),
+    ERROR_102("Invalid GraphQL field Type ''{0}'' provided for the GraphQL field ''{1}''"),
+    ERROR_103("Invalid input parameter type ''{0}'' used in GraphQL field ''{1}''"),
+    ERROR_104("Return type not provided for the GraphQL field ''{0}''"),
+    ERROR_105("The GraphQL field ''{0}'' has a ballerina union type, but the union does not contain any valid data type"
+                      + " to return"),
+    ERROR_106("Invalid resource method accessor ''{0}'' used in method ''{1}''. Only " + RESOURCE_FUNCTION_GET
+                      + " allowed"),
     ERROR_107("A GraphQL service cannot be attached to multiple listeners"),
-    ERROR_108("The GraphQL field `{0}` only returns an error type. It must return a data type"),
+    ERROR_108("The GraphQL field ''{0}'' only returns an error type. It must return a data type"),
     ERROR_109("http:Listener and graphql:ListenerConfiguration are mutually exclusive"),
-    ERROR_110("Invalid GraphQL union type member `{0}` found. Only distinct service types are allowed"),
-    ERROR_111("The GraphQL field name `{0}` is not valid. Name `{1}` must not begin with \"" + DOUBLE_UNDERSCORES +
-                      "\", which is reserved by GraphQL introspection"),
-    ERROR_112("Invalid type found in the GraphQL field `{0}`. A GraphQL field cannot have \"any\" or \"anydata\" as "
+    ERROR_110("Invalid GraphQL union type member ''{0}'' found. Only distinct service types are allowed"),
+    ERROR_111("The GraphQL field name ''{0}'' is not valid. Name ''{1}'' must not begin with \"" + DOUBLE_UNDERSCORES
+                      + "\", which is reserved by GraphQL introspection"),
+    ERROR_112("Invalid type found in the GraphQL field ''{0}''. A GraphQL field cannot have \"any\" or \"anydata\" as "
                       + "the type, instead use specific types"),
-    ERROR_113("A GraphQL service must have at least one resource method with a `" + RESOURCE_FUNCTION_GET + "` "
+    ERROR_113("A GraphQL service must have at least one resource method with a ''" + RESOURCE_FUNCTION_GET + "'' "
                       + "accessor"),
-    ERROR_114("The GraphQL field `{0}` use input type `{1}` as an output type. A GraphQL field cannot use an input "
+    ERROR_114("The GraphQL field ''{0}'' use input type ''{1}'' as an output type. A GraphQL field cannot use an input "
                       + "type as an output type"),
-    ERROR_115("The GraphQL field `{0}` use output type `{1}` as an input type. A GraphQL field cannot use an output "
-                      + "type as an input type"),
-    ERROR_116("The graphql:Context should be the first parameter of method `{0}`"),
-    ERROR_117("Found path parameters `{0}` in GraphQL resource. Path parameters are not allowed in GraphQL resources"),
-    ERROR_118("Invalid resource path `{0}` found in GraphQL resource"),
-    ERROR_119("The graphql:Upload cannot be used as an input type of resource method `{0}`"),
-    ERROR_120("Multidimensional graphql:Upload array parameter found in method `{0}`. GraphQL input cannot have "
+    ERROR_115("The GraphQL field ''{0}'' use output type ''{1}'' as an input type. A GraphQL field cannot use an output"
+                      + " type as an input type"),
+    ERROR_116("The graphql:Context should be the first parameter of method ''{0}''"),
+    ERROR_117("Found path parameters ''{0}'' in GraphQL resource. Path parameters are not allowed in GraphQL "
+                      + "resources"),
+    ERROR_118("Invalid resource path ''{0}'' found in GraphQL resource"),
+    ERROR_119("The graphql:Upload cannot be used as an input type of resource method ''{0}''"),
+    ERROR_120("Multidimensional graphql:Upload array parameter found in method ''{0}''. GraphQL input cannot have "
                       + "multidimensional graphql:Upload arrays"),
-    ERROR_121("Graphql input type must not be a subtype of `error?`"),
+    ERROR_121("Graphql input type must not be a subtype of ''error?''"),
     ERROR_122("Invalid union type for GraphQL input type"),
-    ERROR_123("Non-distinct service class `{0}` is used as a GraphQL interface implementation"),
-    ERROR_124("Invalid hierarchical resource path `{0}` found in subscribe resource"),
-    ERROR_125("Invalid return type `{0}` found in subscribe resource `{1}`. GraphQL subscribe resource must return "
-                      + "`stream` type"),
-    ERROR_126("Invalid GraphQL resource accessor `{0}` found in resource `{1}`. Only " + RESOURCE_FUNCTION_GET +
-                      " and " + RESOURCE_FUNCTION_SUBSCRIBE + " are allowed"),
+    ERROR_123("Non-distinct service class ''{0}'' is used as a GraphQL interface implementation"),
+    ERROR_124("Invalid hierarchical resource path ''{0}'' found in subscribe resource"),
+    ERROR_125("Invalid return type ''{0}'' found in subscribe resource ''{1}''. GraphQL subscribe resource must return "
+                      + "''stream'' type"),
+    ERROR_126("Invalid GraphQL resource accessor ''{0}'' found in resource ''{1}''. Only " + RESOURCE_FUNCTION_GET
+                      + " and " + RESOURCE_FUNCTION_SUBSCRIBE + " are allowed"),
     ERROR_127("Failed to generate the schema from the service. {0}"),
-    ERROR_128("Invalid resource method `{0}` found in GraphQL interceptor. GraphQL interceptors can not have resource "
-                      + "methods"),
-    ERROR_129("Invalid remote method `{0}` found in interceptor service. Only \"execute\" remote method is allowed"),
-    ERROR_130("Anonymous record `{0}` cannot be used as the type of the field `{1}`"),
-    ERROR_131("Anonymous record `{0}` cannot be used as an input object type of the field `{1}`"),
-    ERROR_132("Invalid return type `{0}` provided for the GraphQL field `{1}`. `{0}` is not a service class");
+    ERROR_128("Invalid resource method ''{0}'' found in GraphQL interceptor. GraphQL interceptors can not have resource"
+                      + " methods"),
+    ERROR_129("Invalid remote method ''{0}'' found in interceptor service. Only \"execute\" remote method is allowed"),
+    ERROR_130("Anonymous record ''{0}'' cannot be used as the type of the field ''{1}''"),
+    ERROR_131("Anonymous record ''{0}'' cannot be used as an input object type of the field ''{1}''"),
+    ERROR_132("Invalid return type ''{0}'' provided for the GraphQL field ''{1}''. ''{0}'' is not a service class");
 
     private final String message;
 
