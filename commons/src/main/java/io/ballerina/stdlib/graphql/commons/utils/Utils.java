ef52a0882220a1a0432b7e9ff23d2eaad5f572e0
MohamedSabthar <sabthar@wso2.com>
2023-02-04 21:48:28 +0530

    Move removeEscapeCharacter method to TypeUtils class

--- commons/src/main/java/io/ballerina/stdlib/graphql/commons/utils/Utils.java
+++ commons/src/main/java/io/ballerina/stdlib/graphql/commons/utils/Utils.java
@@ -34,68 +34,14 @@ import io.ballerina.compiler.syntax.tree.TypeDescriptorNode;
 import io.ballerina.compiler.syntax.tree.TypedBindingPatternNode;
 import io.ballerina.projects.plugins.SyntaxNodeAnalysisContext;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
- * Utility class for Ballerina GraphQL schema types.
+ * Utility class to validate GraphQL package services and symbols.
  */
 public class Utils {
 
     public static final String PACKAGE_NAME = "graphql";
     public static final String PACKAGE_ORG = "ballerina";
     public static final String SERVICE_NAME = "Service";
-
-    private static final String UNICODE_REGEX = "\\\\(\\\\*)u\\{([a-fA-F0-9]+)\\}";
-    private static final Pattern UNICODE_PATTERN = Pattern.compile(UNICODE_REGEX);
-
-    private static final String SINGLE_QUOTE_CHARACTER = "'";
-
-    public static String removeEscapeCharacter(String identifier) {
-        if (identifier == null) {
-            return null;
-        }
-
-        Matcher matcher = UNICODE_PATTERN.matcher(identifier);
-        StringBuffer buffer = new StringBuffer(identifier.length());
-        while (matcher.find()) {
-            String leadingSlashes = matcher.group(1);
-            if (isEscapedNumericEscape(leadingSlashes)) {
-                // e.g. \\u{61}, \\\\u{61}
-                continue;
-            }
-
-            int codePoint = Integer.parseInt(matcher.group(2), 16);
-            char[] chars = Character.toChars(codePoint);
-            String ch = String.valueOf(chars);
-
-            if (ch.equals("\\")) {
-                // Ballerina string unescaping is done in two stages.
-                // 1. unicode code point unescaping (doing separately as [2] does not support code points > 0xFFFF)
-                // 2. java unescaping
-                // Replacing unicode code point of backslash at [1] would compromise [2]. Therefore, special case it.
-                matcher.appendReplacement(buffer, Matcher.quoteReplacement(leadingSlashes + "\\u005C"));
-            } else {
-                matcher.appendReplacement(buffer, Matcher.quoteReplacement(leadingSlashes + ch));
-            }
-        }
-        matcher.appendTail(buffer);
-        String value = String.valueOf(buffer);
-
-        if (value.startsWith(SINGLE_QUOTE_CHARACTER)) {
-            return value.substring(1);
-        }
-        return value;
-    }
-
-    private static boolean isEscapedNumericEscape(String leadingSlashes) {
-        return !isEven(leadingSlashes.length());
-    }
-
-    private static boolean isEven(int n) {
-        // (n & 1) is 0 when n is even.
-        return (n & 1) == 0;
-    }
 
     public static boolean isGraphqlService(SyntaxNodeAnalysisContext context) {
         ServiceDeclarationNode node = (ServiceDeclarationNode) context.node();
